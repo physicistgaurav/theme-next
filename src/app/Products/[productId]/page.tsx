5585a0adc3e297209556d1e3b0136a170ea29a0f
physicistgaurav <gauravrijal03@gmail.com>
2024-01-10 16:04:04 +0545

    ui changes

--- src/app/Products/[productId]/page.tsx
+++ src/app/Products/[productId]/page.tsx
@@ -4,29 +4,28 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useProductsStore } from "../../../../zustand/store";
 
-const ProductDetail = ({ params }: {params: { productId: number }}) => {
-  console.log("first", params);
-//   const router = useRouter();
-  const { productId } = params.productId;
+const ProductDetail = ({ params }: { params: { productId: any } }) => {
+  const { productId } = params;
 
+  const [isChecked, setIsChecked] = useState(true);
+
+  const handleInputChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  // from zustand
   const products = useProductsStore((state: any) => state.data);
   const getProducts = useProductsStore((state: any) => state.getProducts);
 
   useEffect(() => {
     getProducts();
   }, [getProducts]);
-  const product = products.find((product: any) => {
-    console.log("Product ID in products array:", product.id);
-    console.log("Product ID from router:", productId);
-    console.log("Equality check:", product.id === productId);
-    return product.id === productId;
-  });
 
-  console.log("productId", productId);
-  console.log("products", products);
-  console.log("one product", product);
+  const product = products.find(
+    (product: any) => product.id === Number(productId)
+  );
 
-  if (!products) {
+  if (!product) {
     return <div>Loading...</div>;
   }
 
@@ -72,7 +71,7 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                     aria-current="page"
                   >
                     {" "}
-                    Coffee{" "}
+                    {product.title}{" "}
                   </a>
                 </div>
               </div>
@@ -87,10 +86,11 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                 <div className="max-w-xl overflow-hidden rounded-lg">
                   <Image
                     className="h-full w-full max-w-full object-cover"
-                    src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                    src={product.image}
                     alt=""
                     width={200}
                     height={200}
+                    priority
                   />
                 </div>
               </div>
@@ -103,10 +103,11 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   >
                     <Image
                       className="h-full w-full max-w-full object-cover"
-                      src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                      src={product.image}
                       alt=""
                       width={200}
                       height={200}
+                      priority
                     />
                   </button>
                   <button
@@ -115,10 +116,11 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   >
                     <Image
                       className="h-full w-full max-w-full object-cover"
-                      src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                      src={product.image}
                       alt=""
                       width={200}
                       height={200}
+                      priority
                     />
                   </button>
                   <button
@@ -127,10 +129,11 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   >
                     <Image
                       className="h-full w-full max-w-full object-cover"
-                      src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                      src={product.image}
                       alt=""
                       width={200}
                       height={200}
+                      priority
                     />
                   </button>
                 </div>
@@ -140,7 +143,7 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
 
           <div className="lg:col-span-2 lg:row-span-2 lg:row-end-2">
             <h1 className="sm: text-2xl font-bold text-gray-900 sm:text-3xl">
-              Afro-Brazillian Coffee
+              {product.title}
             </h1>
 
             <div className="mt-5 flex items-center">
@@ -156,57 +159,13 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                     className=""
                   ></path>
                 </svg>
-                <svg
-                  className="block h-4 w-4 align-middle text-yellow-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                    className=""
-                  ></path>
-                </svg>
-                <svg
-                  className="block h-4 w-4 align-middle text-yellow-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                    className=""
-                  ></path>
-                </svg>
-                <svg
-                  className="block h-4 w-4 align-middle text-yellow-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                    className=""
-                  ></path>
-                </svg>
-                <svg
-                  className="block h-4 w-4 align-middle text-yellow-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-                    className=""
-                  ></path>
-                </svg>
               </div>
               <p className="ml-2 text-sm font-medium text-gray-500">
-                1,209 Reviews
+                {product.rating.count} Reviews
               </p>
             </div>
 
-            <h2 className="mt-8 text-base text-gray-900">Coffee Type</h2>
+            <h2 className="mt-8 text-base text-gray-900">Category</h2>
             <div className="mt-3 flex select-none flex-wrap items-center gap-1">
               <label className="">
                 <input
@@ -214,13 +173,14 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   name="type"
                   value="Powder"
                   className="peer sr-only"
-                  checked
+                  checked={isChecked}
+                  onChange={handleInputChange}
                 />
                 <p className="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">
-                  Powder
+                  {product.category}
                 </p>
               </label>
-              <label className="">
+              {/* <label className="">
                 <input
                   type="radio"
                   name="type"
@@ -228,10 +188,10 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   className="peer sr-only"
                 />
                 <p className="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">
-                  Whole Bean
+                  new laBEL
                 </p>
-              </label>
-              <label className="">
+              </label> */}
+              {/* <label className="">
                 <input
                   type="radio"
                   name="type"
@@ -239,9 +199,9 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   className="peer sr-only"
                 />
                 <p className="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">
-                  Groud
+                  NEW LABEL
                 </p>
-              </label>
+              </label> */}
             </div>
 
             <h2 className="mt-8 text-base text-gray-900">
@@ -253,7 +213,8 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   type="radio"
                   name="subscription"
                   value="4 Months"
-                  className="peer sr-only"
+                  checked={isChecked}
+                  onChange={handleInputChange}
                 />
                 <p className="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">
                   4 Months
@@ -265,8 +226,8 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   type="radio"
                   name="subscription"
                   value="8 Months"
-                  className="peer sr-only"
-                  checked
+                  checked={isChecked}
+                  onChange={handleInputChange}
                 />
                 <p className="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">
                   8 Months
@@ -278,7 +239,9 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   type="radio"
                   name="subscription"
                   value="12 Months"
-                  className="peer sr-only"
+                  // className="peer sr-only"
+                  checked={isChecked}
+                  onChange={handleInputChange}
                 />
                 <p className="peer-checked:bg-black peer-checked:text-white rounded-lg border border-black px-6 py-2 font-bold">
                   12 Months
@@ -289,8 +252,8 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
 
             <div className="mt-10 flex flex-col items-center justify-between space-y-4 border-t border-b py-4 sm:flex-row sm:space-y-0">
               <div className="flex items-end">
-                <h1 className="text-3xl font-bold">$60.50</h1>
-                <span className="text-base">/month</span>
+                <h1 className="text-3xl font-bold">${product.price}</h1>
+                {/* <span className="text-base">/month</span> */}
               </div>
 
               <button
@@ -303,11 +266,11 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
-                  stroke-width="2"
+                  strokeWidth="2"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
                   />
                 </svg>
@@ -325,9 +288,9 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                     className=""
                   ></path>
@@ -344,9 +307,9 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M3 10h18M7 15h1m4 0h1m-7 4h12a3 3 0 003-3V8a3 3 0 00-3-3H6a3 3 0 00-3 3v8a3 3 0 003 3z"
                     className=""
                   ></path>
@@ -376,7 +339,7 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
                   Reviews
                   <span className="ml-2 block rounded-full bg-gray-500 px-2 py-px text-xs font-bold text-gray-100">
                     {" "}
-                    1,209{" "}
+                    {product.rating.count}{" "}
                   </span>
                 </a>
               </nav>
@@ -384,22 +347,8 @@ const ProductDetail = ({ params }: {params: { productId: number }}) => {
 
             <div className="mt-8 flow-root sm:mt-12">
               <h1 className="text-3xl font-bold">Delivered To Your Door</h1>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
-                accusantium nesciunt fuga.
-              </p>
-              <h1 className="mt-8 text-3xl font-bold">
-                From the Fine Farms of Brazil
-              </h1>
-              <p className="mt-4">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-                numquam enim facere.
-              </p>
-              <p className="mt-4">
-                Amet consectetur adipisicing elit. Optio numquam enim facere.
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore
-                rerum nostrum eius facere, ad neque.
-              </p>
+              <p className="mt-4">{product.title}</p>
+              <p className="mt-4">{product.description}</p>
             </div>
           </div>
         </div>
--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -29,29 +29,25 @@ const ProductsScreen = () => {
           >
             <div className="relative  m-10 flex justify-between w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
               <div className="flex flex-col">
-                <a
-                  className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl"
-                  href="#"
-                >
+                <div className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl">
                   <Image
-                    className="object-contain  h-full mx-auto w-2/3 flex justify-center items-center"
+                    className="object-cover  h-full mx-auto w-2/3 flex justify-center items-center"
                     width={100}
                     height={50}
                     quality={95}
                     priority
-                    objectFit="cover"
                     src={product.image}
                     alt="product image"
                   />
                   {/* <span className="absolute top-0 left-0 m-2 rounded-full bg-black px-2 text-center text-sm font-medium text-white">
                   {product.category}
                 </span> */}
-                </a>
-                <a href="#">
+                </div>
+                <div>
                   <h5 className="text-xl mt-3 mx-5 tracking-tight text-slate-900">
                     {product.title}
                   </h5>
-                </a>
+                </div>
               </div>
               <div className="mt-4 px-5 pb-5">
                 <div className="mt-2 mb-5 flex items-center justify-between">
@@ -68,26 +64,23 @@ const ProductsScreen = () => {
                     </span>
                   </div>
                 </div>
-                <a
-                  href="#"
-                  className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300"
-                >
+                <div className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="mr-2 h-6 w-6"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
-                    stroke-width="2"
+                    strokeWidth="2"
                   >
                     <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                     />
                   </svg>
                   Add to cart
-                </a>
+                </div>
               </div>
             </div>
           </Link>
